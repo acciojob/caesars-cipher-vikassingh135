05b5b67f41ff691e9cd9fcaa856674cb8ad371b0
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-13 09:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ function rot13(encodedStr){
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
