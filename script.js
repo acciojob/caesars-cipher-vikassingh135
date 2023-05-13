5b0973aa385236f2c647f61b5679a81556cff766
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-13 09:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ const lookup = {
 
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   let decodedArr = ""; // Your Result goes here
   // Only change code below this line
 	for(let i=0; i<encodedStr.length; i++) {
 		if(!lookup[encodedStr[i]]) {
-			decodedArr.push(encodedStr[i]);
+			decodedArr += (encodedStr[i]);
 		}
-		else decodedArr.push(lookup[encodedStr[i]]);
+		else decodedArr += (lookup[encodedStr[i]]);
 	}
 
   return decodedArr;
@@ -27,6 +27,6 @@ function rot13(encodedStr){
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(rot13("SERR YBIR? NPPVBWBO"));
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
